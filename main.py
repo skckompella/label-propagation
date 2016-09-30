1c179d1e80101cb510e6250afbe63f359d327ae1
skckompella <skckompella@gmail.com>
2016-09-30 19:23:25 -0400

    Corrected code and adding kin for comparison

--- main.py
+++ main.py
@@ -1,88 +1,70 @@
 import scipy.io
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from sklearn import neighbors
 from sklearn.neighbors import kneighbors_graph
-import matplotlib.pyplot as plt
 
-def label_propagate(affinity_mat, row_sum_diag_mat, label_ids, labels):
-    print " > Initializing......",
+def check_accuracy(predicted_labels, given_labels, initially_labeled=0):
+    right_predictions = np.sum(predicted_labels == given_labels)
+    return float(right_predictions+initially_labeled)/given_labels.shape[0]
+
+
+def label_propagate(rand_walk_mat, labels, label_ids):
     Y = np.zeros(labels.shape)
-    for i in l:
+    for i in label_ids:
         Y[i] = labels[i]
-    print "[done]"
 
-
-    rand_walk_mat = np.linalg.inv(row_sum_diag_mat).dot(affinity_mat)
-    #print label_ids
     for i in range(0,1000):
-        #print " Itr: " + str(i) + "\r"
         Y = rand_walk_mat.dot(Y)
-        for i in labeled_ids:
+        for i in label_ids:
             Y[i] = labels[i]  # Clamping
-        #row_sum = Y_mod.sum(axis=0)
-    #if abs(row_sum) < 0.003:
-     #   return Y
+    Y[Y<=0] = -1
+    Y[Y>0] = 1
     return Y
 
-def knn():
-    return
+def knn(feature_mat, labels, ids):
+    ft = np.ndarray(shape=(ids.shape[0],feature_mat.shape[1]))
+    l = np.ndarray(shape=(ids.shape[0],1))
+    for i in range(0, ids.shape[0]):
+        ft[i] = feature_mat[ids[i]]
+        l[i] = labels[ids[i]]
+
+
+    knn = neighbors.KNeighborsClassifier()
+    knn.fit(ft, l.ravel())
+    predictions = knn.predict(feature_mat[10:])
+    predictions = predictions.reshape((labels[10:].shape[0], 1)) #change from (1490, ) to (1490, 1)
+    return predictions
 
 
 ## Main##
 
-print " > Loading data......",
 data = scipy.io.loadmat('SSL,set=9,data.mat')
 feature_mat = data['X'].toarray()
 labels = data["y"]
-print "[done]"
-
-print " > Loading data2.....",
 
 data2 = scipy.io.loadmat('SSL,set=9,splits,labeled=10.mat')
 labeled_ids = data2['idxLabs']
 unlabeled_ids = data2['idxUnls']
-print "[done]"
 
-print " > Computing W.......",
 affinity_mat = kneighbors_graph(feature_mat, 5, mode='connectivity', include_self=True).toarray()
 affinity_mat = affinity_mat + affinity_mat.transpose() ## To make the matrix symmetric
 affinity_mat[np.nonzero(affinity_mat)] = 1 ## any non zero value has to be 1
-print "[done]"
 
-print " > Computing D.......",
 row_sums = affinity_mat.sum(axis=1)
 row_sum_diag_mat = np.diag(row_sums)
-print "[done]"
 
-accuracy = []
+rand_walk_mat = np.linalg.inv(row_sum_diag_mat).dot(affinity_mat)
 
-for l in labeled_ids:
-    print " > Beginning Label prop..."
-    Y_mod = label_propagate(affinity_mat, row_sum_diag_mat, l, labels)
-    y = Y_mod
-    a = 0
-    negc = 0
-    posc = 0
-    for i in range(Y_mod.shape[0]):
-        if Y_mod[i] <= 0:
-            y[i] = -1
-            negc += 1
-            if labels[i] ==  -1:
-                a += 1
-        else:
-            y[i] = 1
-            posc += 1
-            if labels[i] ==  1:
-                a += 1
-    print "Neg: " + str(negc)
-    print "Pos: " + str(posc)
-    print "Accuracy = " + str(float(a)/1500)
-    accuracy.append(a)
+lp_accuracy = []
+knn_accuracy = []
 
+for split in labeled_ids: #Each split is a sequence of label IDs
+    lp_predictions = label_propagate(rand_walk_mat, labels, split)
+    lp_accuracy.append(check_accuracy(lp_predictions, labels))
+    knn_predictions = knn(feature_mat, labels, split)
+    knn_accuracy.append(check_accuracy(knn_predictions, labels[10:], 10))
 
-#x = [i for i in range(0,1500)]
-
-#fig2 = plt.figure()
-#bx = plt.gca()
-#bx.scatter(x, Y_mod)
-#plt.show()
+print "Label proagation accuracy: "
+print lp_accuracy
+print "KNN Accuracy: "
+print knn_accuracy
